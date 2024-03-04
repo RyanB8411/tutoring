b705290d988482b4e108e11e75f0b95ad01ce67f
Ryan <RyanABurkhardt@gmail.com>
2024-03-03 22:14:21 -0500

    finished session

---
+++ ses.py
@@ -0,0 +1,13 @@
+print("Ticket Kiosk")
+
+age = int(input("Enter age: "))
+
+if age < 15:
+  
+  print("The entrance price is $20")
+  
+elif age >= 15:
+  
+  print("The entrance price is $40")
+  
+      
