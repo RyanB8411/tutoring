ef5a24f23b1a15d7ef6e6484a87074b8c78dbd6f
Ryan <RyanABurkhardt@gmail.com>
2024-03-07 20:33:22 -0500

    Worked with student on if else statements and print line statements

--- ses.py
+++ ses.py
@@ -31,12 +31,25 @@
 
 # print(f"\nCongratulation! You won on round {NumRounds}! With {BankPoints} points!")
 
-time = input("Is it currently day or night?:" )
-if time == 'night':
-  stars = input("Can you see the stars?: ")
-  if stars == "yes":
-    print ("Oh wow! I wonder how many there are...")
+# time = input("Is it currently day or night?:" )
+# if time == 'night':
+#   stars = input("Can you see the stars?: ")
+#   if stars == "yes":
+#     print ("Oh wow! I wonder how many there are...")
+#   else:
+#     print ("Oh, that's a shame, the stars are so beautiful")
+# else:
+#   print ("I hope the sun is shining on you!")
+  
+#For this example we will have a user enter a room and either go left or right but have an if or else stement to decide
+in_room = True
+while in_room:
+  direction = input("Enter a direction")
+  if direction.lower() == "left":
+    print("You went left successfully")
+    in_room = False
+  elif direction.lower() == "right":
+    print("You sucessfully went right")
+    in_room = False
   else:
-    print ("Oh, that's a shame, the stars are so beautiful")
-else:
-  print ("I hope the sun is shining on you!")
+    print("Invalid input try again.")
